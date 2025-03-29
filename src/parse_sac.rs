b61b48f48493715355e1b79415b9fa1bbfbbfdf5
ExplodingImplosion <explodingimplosion14@gmail.com>
2025-03-29 14:53:43 -0400

    remove v3

--- src/parse_sac.rs
+++ src/parse_sac.rs
@@ -61,18 +61,6 @@ pub struct ParsedDemo {
     pub blue_off_last_ticks: Vec<DemoTick>,
 }
 
-pub struct Vector3 {
-    x: f32,
-    y: f32,
-    z: f32,
-}
-
-impl Vector3{
-    pub fn new(x: f32, y: f32, z: f32) -> Vector3 {
-        Vector3{x, y, z}
-    }
-}
-
 impl ParsedDemo {
 
     pub fn new(header: Header) -> Self {
