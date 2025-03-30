61bdb7ca9d60ae21b7f9c869d7e0ce7a4a5d653f
ExplodingImplosion <explodingimplosion14@gmail.com>
2025-03-29 21:10:10 -0400

    for random other demo

--- run for 6s demo.bat
+++ run for 6s demo.bat
@@ -1,2 +1,2 @@
-C:/Users/explo/.cargo/bin/cargo.exe run --package tf-demo-parser --bin parse_demo "D:/Productivity/Projects/capstone-demo-parser/bert_6s_demo.dem" --profile dev > lmao.txt
+C:/Users/explo/.cargo/bin/cargo.exe run --package tf-demo-parser --bin parse_demo "D:/Productivity/Projects/capstone-demo-parser/random_other_demo.dem" --profile dev > lmao.txt
 pause
